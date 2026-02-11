88deb7c648a810b2de19db62da1610c1414a074d
marciel83 <148016279+marciel83@users.noreply.github.com>
2026-02-11 00:05:36 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import path from 'path'
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
+  const env = loadEnv(mode, '.', '')
 
   return {
-    base: '/novafinance/',
+    base: '/NovaFinance/',
+
+    plugins: [react()],
 
     server: {
       port: 3000,
       host: '0.0.0.0',
     },
-
-    plugins: [react()],
 
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
@@ -25,5 +25,5 @@ export default defineConfig(({ mode }) => {
         '@': path.resolve(__dirname, '.'),
       },
     },
-  };
-});
+  }
+})
