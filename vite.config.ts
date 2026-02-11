e457298d3531f9ef57825d82b4a1bbe695e79ed7
marciel83 <148016279+marciel83@users.noreply.github.com>
2026-02-11 00:00:26 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
 
   return {
-    base: '/NOME-DO-REPO/',
+    base: '/novafinance/',
 
     server: {
       port: 3000,
